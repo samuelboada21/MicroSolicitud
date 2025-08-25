24794fb3c456c748e72d4af997f18323b9be2315
samuelboada21 <samuelomarboba@ufps.edu.co>
2025-08-25 14:01:48 -0500

    Configuración inicial del proyecto en spring initializer

---
+++ src/main/java/com/solicitud/solicitud/SolicitudApplication.java
@@ -0,0 +1,13 @@
+package com.solicitud.solicitud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitudApplication.class, args);
+	}
+
+}
